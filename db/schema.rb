ba379d1c271f1b3e231d771d82a6c2b7b513f2e0
yosino26 <fn.yoshino57@gmail.com>
2025-08-03 03:18:59 +0900

    Commentモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_31_153253) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_02_181609) do
+  create_table "comments", charset: "utf8mb3", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", charset: "utf8mb3", force: :cascade do |t|
     t.string "text"
     t.text "image"
