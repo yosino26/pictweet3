8023d2e0e5ae0e251406a1dc2d84143da813d346
yosino26 <fn.yoshino57@gmail.com>
2025-08-03 03:26:54 +0900

    ネストを利用したルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
-  resources :tweets
   resources :users, only: :show
+   
+  resources :tweets do
+    resources :comments, only: :create
+  end
 end
