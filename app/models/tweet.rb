1c5d018b943dc14c22da5634c4782eb54f0ab5ab
yosino26 <fn.yoshino57@gmail.com>
2025-08-04 01:59:55 +0900

    検索のメソッドをモデルし、searchアクションをコントローラーに定義

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,12 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
   has_many :comments  # commentsテーブルとのアソシエーション　追記
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
 end
